7d064d405408e0f5bd99b0bf7ae1d5805bf38183
Babis <cwbabis@gmail.com>
2019-06-10 19:44:11 -0700

    testing mobile responsiveness

--- src/components/ChampionCard/index.js
+++ src/components/ChampionCard/index.js
@@ -1,16 +1,13 @@
 import React from "react";
 import "./style.css";
 
-const imgStyling = {
-  height: "70px",
-  width: "70px"
-};
+
 
 function ChampionCard(props) {
   return (
     <div className="card" onClick={() => props.handleClick(props.id)} >
       <div className="img-container">
-        <img style={imgStyling} alt={props.name} src={props.image} />
+        <img className="imgStyling" alt={props.name} src={props.image} />
       </div>
     </div>
   );
